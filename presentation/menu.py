87c6d6c389ff53cf7df014ad7106c986aeabd3e0
Peter Pallen <42099752+petepall@users.noreply.github.com>
2019-06-20 17:16:34 +0200

    renamed functions for clarity

--- presentation/menu.py
+++ presentation/menu.py
@@ -9,20 +9,20 @@ class StoreMenu:
     colorama.init()
 
     @classmethod
-    def _header(cls) -> None:
+    def _print_header(cls) -> None:
         text = Figlet(font='speed', width=100)
         print(text.renderText("Store Inventory"))
         print(f"{'~' * 96:96}")
 
     @classmethod
-    def _menu_options(cls, options: OrderedDict) -> None:
+    def _show_menu_options(cls, options: OrderedDict) -> None:
         for key, value in options.items():
             print(f"   {Fore.YELLOW}{key}){Fore.RESET} {value.strip():90}",
                   end="\n")
         print()
 
     @classmethod
-    def _process_menu_input(cls):
+    def _process_menu_selection(cls):
         pass
 
     @classmethod
