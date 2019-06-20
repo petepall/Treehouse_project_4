9576208f43eab742cfa531eec5bff2c76a58f28d
Peter Pallen <42099752+petepall@users.noreply.github.com>
2019-06-20 14:21:09 +0200

    Added check on db folder existence.

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import os
-from pathlib import Path
 
 from peewee import InternalError
 
 from models.base_model import dbhandle
 from models.product import Product
+from utilities.constants import CSV_FILE_IMPORT, DB_FILE, DB_FOLDER
 from utilities.csv_processing import clean_csv_data, read_from_csv
 from utilities.db_processing import write_rows_to_db
-from utilities.helper_functions import clear_screen
-
-DB_FILE = f"{Path().absolute()}\\db\\inventory.db"
-CSV_FILE = f"{Path().absolute()}\\import_data\\inventory.csv"
+from utilities.helper_functions import (check_folder_exists, clear_screen,
+                                        create_folder)
 
 
 def create_database():
@@ -23,7 +21,7 @@ def create_database():
 
 
 def load_csv_data_in_database():
-    loaded_csv = read_from_csv(CSV_FILE)
+    loaded_csv = read_from_csv(CSV_FILE_IMPORT)
     csv_data = clean_csv_data(loaded_csv)
     dbhandle.connect()
     write_rows_to_db(csv_data)
@@ -31,12 +29,16 @@ def load_csv_data_in_database():
 
 
 if __name__ == "__main__":
-    if not os.path.isfile(DB_FILE):
-        create_database()
+    if not os.path.isfile((DB_FOLDER + DB_FILE)):
+        if check_folder_exists(DB_FOLDER):
+            create_database()
+        else:
+            create_folder(DB_FOLDER)
+            create_database()
 
-    if os.path.isfile(CSV_FILE):
+    if os.path.isfile(CSV_FILE_IMPORT):
         load_csv_data_in_database()
     else:
-        print(f"The {CSV_FILE} does not exist")
+        print(f"The {CSV_FILE_IMPORT} does not exist")
 
     clear_screen()
