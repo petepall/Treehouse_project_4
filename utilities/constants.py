2b259eb52f7b9cabde0a158e43369a7d9feaf484
Peter Pallen <42099752+petepall@users.noreply.github.com>
2019-06-20 14:23:51 +0200

    Moved constants to separate file

---
+++ utilities/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+DB_FOLDER = f"{Path().absolute()}\\db\\"
+DB_FILE = "inventory.db"
+CSV_FILE_IMPORT = f"{Path().absolute()}\\import_data\\inventory.csv"
+CSV_FOLDER_EXPORT = f"{Path().absolute()}\\archive\\"
+CSV_FILE_EXPORT = "backup.csv"
